40772d8232cf4052edc1829395f681ed7694a8c3
Pedro Reis <pedrohsareis@gmail.com>
2018-08-29 19:59:45 -0300

    Cria um intent para professorActivity

--- app/src/main/java/br/ufjf/dcc196/exemplo02/MainActivity.java
+++ app/src/main/java/br/ufjf/dcc196/exemplo02/MainActivity.java
@@ -1,13 +1,27 @@
 package br.ufjf.dcc196.exemplo02;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnProfessor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnProfessor = findViewById(R.id.btn_professor);
+
+        btnProfessor.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ProfessorActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
