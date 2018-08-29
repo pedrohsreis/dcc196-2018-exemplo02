307d482b7c43f1418a63ed61590a23ab8f24c468
Pedro Reis <pedrohsareis@gmail.com>
2018-08-29 20:35:22 -0300

    Adiciona notificação toast para estado da ProfessorActivity

--- app/src/main/java/br/ufjf/dcc196/exemplo02/ProfessorActivity.java
+++ app/src/main/java/br/ufjf/dcc196/exemplo02/ProfessorActivity.java
@@ -3,8 +3,9 @@ package br.ufjf.dcc196.exemplo02;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class ProfessorActivity extends AppCompatActivity {
+public class ProfessorActivity extends MainActivity {
 
     private TextView txtProfMensagem;
     @Override
@@ -20,6 +21,42 @@ public class ProfessorActivity extends AppCompatActivity {
             String nome = extras.getString(PESSOA_NOME);
             txtProfMensagem.setText("Olá professor " + nome + "!");
         }
+        Toast.makeText(getApplicationContext(),"prof onCreate()", Toast.LENGTH_LONG).show();
+    }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(getApplicationContext(),"prof onStart()", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(getApplicationContext(),"prof onResume()", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(getApplicationContext(),"prof onPause()", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(getApplicationContext(),"prof onStop()", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Toast.makeText(getApplicationContext(),"prof onRestart()", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(getApplicationContext(),"prof onDestroy()", Toast.LENGTH_LONG).show();
     }
 }
